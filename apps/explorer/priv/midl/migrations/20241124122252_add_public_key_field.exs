62310b3d89e76cfc21785ab502f2cee3c415795f
Roman Makarevich <remmybump@gmail.com>
2024-11-25 15:16:23 +0300

    fixed migration

--- apps/explorer/priv/midl/migrations/20241124122252_add_public_key_field.exs
+++ apps/explorer/priv/midl/migrations/20241124122252_add_public_key_field.exs
@@ -2,6 +2,8 @@ defmodule Explorer.Repo.Midl.Migrations.AddPublicKeyField do
   use Ecto.Migration
 
   def change do
-    add(:public_key, :bytea)
+    alter table(:transactions) do
+      add(:public_key, :bytea)
+    end
   end
 end
