6d0cca264ab9970e839cc80a5583c638b895ddcd
Ivan <johnniet19@gmail.com>
2024-12-27 15:47:30 +0100

    Clean out 0x prefix for BTC hashes

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/midl_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/midl_view.ex
@@ -25,9 +25,28 @@ defmodule BlockScoutWeb.API.V2.MidlView do
         }) :: map()
   def extend_transaction_json_response(out_json, %Transaction{} = transaction) do
     out_json
-    |> Map.put("btc_tx_hash", transaction.btc_tx_hash)
-    |> Map.put("public_key", transaction.public_key)
-    |> Map.put("btc_address_byte", transaction.btc_address_byte)
+    |> Map.put("btc_tx_hash", remove_0x_prefix_if_any(transaction.btc_tx_hash))
+    |> Map.put("public_key", remove_0x_prefix_if_any(transaction.public_key))
+    |> Map.put("btc_address_byte", remove_0x_prefix_if_any(transaction.btc_address_byte))
+  end
+
+  def remove_0x_prefix_if_any(nil), do: nil
+
+  @doc """
+    Midl RPC returns BTC parameters: `btc_tx_hash`, `public_key`, `btc_address_byte` with 0x prefix.
+    That is not consistent with the rest of the system, so we remove the prefix here.
+
+    The solution is temporary. Prefix should be cleaned on the RPC side or saving to DB side.
+  """
+  def remove_0x_prefix_if_any(%Explorer.Chain.Hash{} = hash_struct) do
+    # Convert the hash struct to a string, e.g. "0x9e48a19b..."
+    hashed_string = Explorer.Chain.Hash.to_string(hash_struct)
+
+    # If it starts with "0x", remove that prefix
+    case hashed_string do
+      "0x" <> rest -> rest
+      other -> other
+    end
   end
 
 end
